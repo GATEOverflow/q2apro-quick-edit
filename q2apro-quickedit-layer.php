528973f6620da3248e9528421623491456a67eff
Arjun <arjunsuresh1987@gmail.com>
2022-09-11 20:54:02 +0530

    Version 1.1, mathpix option add

--- q2apro-quickedit-layer.php
+++ q2apro-quickedit-layer.php
@@ -3,54 +3,63 @@
 class qa_html_theme_layer extends qa_html_theme_base
 {
 
-
-	function head_script()//add custom js
+	function head_css()//add custom css
 	{
-		qa_html_theme_base::head_script();
-		$version=0.00699;
+		qa_html_theme_base::head_css();
+		$version=0.0071;
 		$url=qa_request();
 		$url_parts=explode('/',$url);
-		foreach($url_parts as $val)
+		if(sizeof($url_parts) > 0 && (strpos($url_parts[0], "quickedit") !== false))
 		{
-			/*Add new pages here, for inclusion*/
-			if($val==='quickedit')// 
-			{
+			$this->output('<link rel="stylesheet" defer type="text/css" href="'.QA_HTML_THEME_LAYER_URLTOROOT.'css/style.css?v='.$version.'">');
 
-				 $this->output('<script> var qa_tags_examples="";');
-		                $this->output('if (typeof qa_tags_complete === "undefined") {var qa_tags_complete =\'\';}');
-                		$template='<a href="#" class="qa-tag-link" onclick="return qa_tag_edit_click(this);">^</a>';
-                		$this->output('var qa_tag_edit_template =\''.$template.'\';');
-                		$this->output('</script>');
+		}
+	}
 
-				$this->output('<script type="text/javascript" src="'.QA_HTML_THEME_LAYER_URLTOROOT.'js/qedit.js?v='.$version.'"></script>');
+	function body_suffix()//add custom js
+	{
+		qa_html_theme_base::body_suffix();
+		$version=0.0071;
+		$url=qa_request();
+		$url_parts=explode('/',$url);
+		if(sizeof($url_parts) > 0 && (strpos($url_parts[0], "quickedit") !== false))
+		{
+			$this->output('<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/jquery-ui.min.js"></script>');
+			$this->output('<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/jquery-ui.css">');
 
-				$this->output(' <script type="text/javascript">
-                                $(document).ready(function(){
+			$this->output('<script> var qa_tags_examples="";');
+			$this->output('if (typeof qa_tags_complete === "undefined") {var qa_tags_complete =\'\';}');
+			$template='<a href="#" class="qa-tag-link" onclick="return qa_tag_edit_click(this);">^</a>';
+			$this->output('var qa_tag_edit_template =\''.$template.'\';');
+			$this->output('</script>');
 
-                                        $(".post_tags").click( function() {
+			$this->output('<script type="text/javascript" src="'.QA_HTML_THEME_LAYER_URLTOROOT.'js/quickedit.js?v='.$version.'"></script>');
+			$this->output(' <script type="text/javascript">
+				$(document).ready(function(){
 
-                                                if(qa_tags_complete == ""){
-                                                $.ajax({
-type: "POST",
-url: "'.qa_path("quickedit_ajax_page").'",
-data: {ajax:"hello" },
-error: function() {
-console.log("server: ajax error");
-},
-success: function(htmldata) {
-qa_tags_complete = htmldata;
-}
-});
-                                                }
-                                                else {
-                                                }
-                                                });
+					$(".post_tags").click( function() {
 
-});
+						if(qa_tags_complete == ""){
+							$.ajax({
+							type: "POST",
+								url: "'.qa_path("quickedit_ajax_page").'",
+								data: {ajax:"hello" },
+								error: function() {
+									console.log("server: ajax error");
+		},
+			success: function(htmldata) {
+				qa_tags_complete = htmldata;
+		}
+		});
+		}
+		else {
+		}
+		});
+
+		});
 </script> 
 ');
 
-			}
 
 		}
 	}
--- q2apro-quickedit-page.php
+++ q2apro-quickedit-page.php
@@ -30,12 +30,12 @@ class q2apro_quickedit {
 	function suggest_requests() 
 	{	
 		return array(
-				array(
-					'title' => 'Quick Tagger Page', // title of page
-					'request' => 'quickedit', // request name
-					'nav' => 'F', // 'M'=main, 'F'=footer, 'B'=before main, 'O'=opposite main, null=none
-				     ),
-			    );
+			array(
+				'title' => 'Quick Categorize', // title of page
+				'request' => 'quickedit', // request name
+				'nav' => 'F', // 'M'=main, 'F'=footer, 'B'=before main, 'O'=opposite main, null=none
+			),
+		);
 	}
 
 	// for url query
@@ -47,9 +47,86 @@ class q2apro_quickedit {
 
 		return false;
 	}
+	function tags_selectspec($min)
+		/*
+		   Return the selectspec to retrieve all the tags and their ids having a min count of $min
+		 */
+	{
+		$count=QA_DB_RETRIEVE_COMPLETE_TAGS;
+		return array(
+			'columns' => array('word',  '^words.wordid as wordid'),
+			'source' => '^words JOIN (SELECT wordid FROM ^words WHERE tagcount>='.$min.' ORDER BY tagcount DESC) y ON ^words.wordid=y.wordid order by word',
+			'arguments' => null,
+			'arraykey' => 'word',
+			'arrayvalue' => 'word',
+			'sort' => 'word',
+		);
+	}
+
+	function tagform()
+	{
+		require_once QA_INCLUDE_DIR.'db/selects.php';
+		$mintags = 1;
+		$tags=qa_db_single_select($this->tags_selectspec($mintags));
+		$fields = array();
+		$fields[] = array(
+			'label' => 'Tag to fetch Post-IDs',
+			'type'=>'select',
+			'tags' => "id='tag_selector' name='tagstring' class='col' onchange='dosubmit(this)'",
+			'options' => $tags,
+		);
+		$userlevel = qa_get_logged_in_level();
+		if($userlevel >= QA_USER_LEVEL_SUPER) {
+			$fields[] = array(
+				'label' => 'Change already updated category to new one as per tag',
+				'note' => 'If unchecked change is limited to uncategorized and in categories: Others, Unknown Category and new',
+				'type'=>'checkbox',
+				'tags' => "id='force' name='force' class='col'",
+				'value' => "",
+			);
+		}
+			$fields[] = array(
+				'label' => 'Convert image to text',
+				'note' => 'If checked will try to append the image text to question content',
+				'type'=>'checkbox',
+				'tags' => "id='ocr' name='ocr' class='col'",
+				'value' => "",
+			);
+		
+
+		$ok = qa_get('ok')?qa_get('ok'):null;
+
+		return array(
+			'ok' => ($ok && !isset($error)) ? $ok : null,
+
+			'fields' => $fields,
+			'tags' => " action='".qa_self_html()."' method='post'",
+			'title' => 'Select a tag to do Category Update',
+			'style' => 'wide',
+			'buttons' => array(
+				array(
+					'label' => 'Submit',
+					'tags' => 'NAME="changesubmit" id="submit_button"',
+					' value' => 'Submit',
+				),
+			),
+		);
+
+
+	}
+
+	function process_submit_ocr($tag) {
+		mathpix_process_ocr($tag, true);
+	}
+	function mysorttitle($a, $b){
+		//return strcmp($a['postid'], $b['postid']);
+		$a_split = explode(":", $a['title']);
+		$b_split = explode(":", $b['title']);
+		if(!strcmp(trim($a_split[0]), trim($b_split[0])) && count($a_split) > 1 && count($b_split) > 1) return trim($a_split[1]) > trim($b_split[1]);
+		return strcmp(trim($a_split[0]), trim($b_split[0]));
+	}
 
 	function process_request($request) {
-
 		if(qa_opt('q2apro_quickedit_enabled')!=1) {
 			$qa_content=qa_content_prepare();
 			$qa_content['error'] = '<div>'.qa_lang_html('q2apro_quickedit_lang/plugin_disabled').'</div>';
@@ -61,94 +138,146 @@ class q2apro_quickedit {
 			$qa_content['error'] = qa_lang_html('q2apro_quickedit_lang/access_forbidden');
 			return $qa_content;
 		}
-
-		// AJAX post: we received post data, so it should be the ajax call to update the tags of the post
-		$transferString = qa_post_text('ajaxdata'); // holds postid, question title, tags
-		if(isset($transferString)) {
-			$newdata = json_decode($transferString,true);
-			$newdata = str_replace('&quot;', '"', $newdata); // see stackoverflow.com/questions/3110487/
-			// echo '# '.$newdata['postid'].' ||| '.$newdata['title'].' ||| '.$newdata['tags']; 	return;
-
-			$postid = $newdata['postid'];
-			$posttitle = $newdata['title'];
-			$posttags = $newdata['tags'];
-
-			if(!isset($postid) || !isset($posttitle) || !isset($posttags)) {
-				echo 'tags='.qa_lang_html('q2apro_quickedit_lang/access_problem');
-				return;
-			}
-			else {
-				require_once QA_INCLUDE_DIR.'app/users.php';
-				$userid = qa_get_logged_in_userid();
-				$tagsIn = str_replace(' ', ',', $posttags); // convert spaces to comma
-				// process new tags
-				require_once QA_INCLUDE_DIR.'qa-app-posts.php';
-				qa_post_set_content($postid, $posttitle, null, null, $tagsIn, null,null, $userid, null, null); 
-				$tags = qa_post_tags_to_tagstring($tagsIn); // correctly parse tags string
-			} // end db update
-
-			// header('Content-Type: text/plain; charset=utf-8');
-			// echo 'updated postid: '.$postid.' with tags: '.$tags;
-
-			// ajax return array data to write back into table
-			$arrayBack = array(
-					'postid' => $postid,
-					'title' => $posttitle,
-					'tags' => $tags
-					);
-			echo json_encode($arrayBack);
-			return;
-		} // end POST data
-
-		/* start */
+		$userid = qa_get_logged_in_userid();
+		$userlevel = qa_get_logged_in_level();
+		$c = 2;
+		$ocr=(bool)qa_post_text('ocr') ;
+		$force=(bool)qa_post_text('force') && $userlevel >= QA_USER_LEVEL_SUPER ;
 		$qa_content=qa_content_prepare();
-		qa_set_template('qp-quickedit-page');
+		if (qa_clicked('changesubmit'))
+		{
+			$tag = qa_post_text('tagstring');
+			//			echo "tag = ".$tag;
+			$filter = ' true ';
+			if(!$force)
+			{
+				$filter=" postid in (select postid from ^posts where categoryid is null or categoryid in(select categoryid from ^categories where title like 'Others' or title like 'Unknown Category' or title like 'new' ))";// and categoryid =6";
+			}
+			$query = "select postid from ^posttags where wordid = (select wordid from ^words WHERE word = '".qa_strtolower($tag)."') and $filter";
+			$result = qa_db_query_sub($query);
+			$postids = qa_db_read_all_values($result, true);
+			$count = 0;
+			foreach ($postids as $postid)
+			{
+
+				$updated = false;
+				$query="select tags from ^posts where postid = #";
+				$result = qa_db_query_sub($query, $postid);
+				$fulltags = qa_db_read_one_value($result, true);
+				$fulltagsarray = qa_tagstring_to_tags($fulltags);
+				$userid = qa_get_logged_in_userid();
+				foreach ($fulltagsarray as $tagvalue)
+				{
+					if($tagvalue === $tag) continue;//skip the selection one. 
+
+					$query = "select categoryid from ^categories where tags like $";
+					$result = qa_db_query_sub($query, $tagvalue);
+					$category = qa_db_read_one_value($result, true);
+					if(!$category) continue;
+					$updated = true;
+					//echo "($postid, $category, $tagvalue, $userid)";
+					//qa_post_set_category($postid, $category);
+					qa_post_set_category($postid, $category, $userid);
+					break;
+				}
+				if($updated) $count++;
+				//	break;
+
+			}
+			$qa_content['custom'.++$c] = "<p>$count posts have been categorized successfully</p>";
+		}
+		if($ocr) {
+			$tag = qa_post_text('tagstring');
+			if($tag)
+			$this -> process_submit_ocr($tag);
+		}
+		/* start */
+		qa_set_template('qp-quickeditcat-page');
 		$qa_content['title'] = qa_lang_html('q2apro_quickedit_lang/page_title'); // page title
 
 		// counter for custom html output
-		$c = 2;
+
+		$qa_content['custom'.++$c] = '<script type="text/javascript">
+			$(function()
+			{
+				if (localStorage.getItem("tag_selector")) {
+					$("#tag_selector option").eq(localStorage.getItem("tag_selector")).prop("selected", true);
+	}
+	});
+
+	function dosubmit(e){ 
+		localStorage.setItem("tag_selector", $("option:selected", e).index());
+		e.form.submit();}</script>';
+
 
 		// do pagination
 		$start = (int)qa_get('start'); // gets start value from URL
 		$pagesize = 500; // items per page
-		$count = qa_opt('cache_qcount'); // items total
-		$qa_content['page_links'] = qa_html_page_links(qa_request(), $start, $pagesize, $count, true); // last parameter is prevnext
-		$tagfilter = null;
+		$tagstring='';
 		if(isset($_GET['tagfilter']))
 			$tagfilter = $_GET['tagfilter'];
-		$tagstring = '';
-		if($tagfilter) $tagstring = " and tags like '%".$tagfilter."%'";
+		$tagfilter = qa_post_text('tagstring');
+		if($tagfilter) {
+			$tagstring = " and a.tags like '%".$tagfilter."%' ";
+			$tagstring = " and a.postid in (select postid from ^posttags where wordid = (select wordid from ^words WHERE word = '$tagfilter' and word = '".qa_strtolower($tagfilter)."'))";
+		}
+		//echo $tagstring;
+		//exit;
+		//else		$tagstring .= " and a.tags NOT LIKE ''"; 
+		#$tagstring .= " and a.categoryid = 6";
 
+		$qa_content['form'] =  $this -> tagform();
 
 		// query to get all posts according to pagination, ignore closed questions
-		$queryAllPosts = qa_db_query_sub('SELECT postid,tags,title,content,format FROM `^posts`
-				WHERE `type` = "Q"
-				AND `closedbyid` IS NULL'. $tagstring.' 
-				ORDER BY postid DESC
-				LIMIT #,#
-				', $start, $pagesize);
+		$queryAllPosts = qa_db_query_sub('SELECT a.postid,a.tags,a.title,a.content,format,b.title as category,c.answer_str as answer FROM `^posts` a 
+			left join ^ec_answers c on a.postid = c.postid 
+			left join ^categories b on a.categoryid = b.categoryid
+			WHERE `type` = "Q" 
+			AND `closedbyid` IS NULL'. $tagstring.' 
+			ORDER BY title 
+			LIMIT #,#
+', $start, $pagesize);
 
 		// initiate output string
-		$tagtable = '<table class="tagtable"> <thead> <tr> <th>'.qa_lang_html('q2apro_quickedit_lang/th_postid').'</th> <th>'.qa_lang_html('q2apro_quickedit_lang/th_questiontitle').'</th> <th>'.qa_lang_html('q2apro_quickedit_lang/th_posttags').'</th> </tr></thead>';
+		$tagtable = '<table class="tagtable" id="quickedittable"> <thead> <tr> <th>No.</th><th>'.qa_lang_html('q2apro_quickedit_lang/th_postid').'</th> <th>Answer</th><th>'.qa_lang_html('q2apro_quickedit_lang/th_questiontitle').'</th><th>'.qa_lang_html('q2apro_quickedit_lang/th_postcategory').'</th> <th>'.qa_lang_html('q2apro_quickedit_lang/th_posttags').'</th> </tr></thead>';
 		$maxlength = qa_opt('mouseover_content_max_len'); // 480
 
 		require_once QA_INCLUDE_DIR.'qa-util-string.php'; // for qa_shorten_string_line()
 		$blockwordspreg=qa_get_block_words_preg();
+		$results = qa_db_read_all_assoc($queryAllPosts);
+		usort($results, array("q2apro_quickeditcat", "mysorttitle"));
+		//usort($results, "mysorttitle");
+		//	print_r($results);
+		//	exit;
+		{
 
-		while ( ($row = qa_db_read_one_assoc($queryAllPosts,true)) !== null ) {
+
+		}
+		$count = count($results); // items total
+		$qa_content['page_links'] = qa_html_page_links(qa_request(), $start, $pagesize, $count, true); // last parameter is prevnext
+		$rowcnt = 0;
+		foreach($results as $row) {
+			//while ( ($row = qa_db_read_one_assoc($queryAllPosts,true)) !== null ) {
 			$text=qa_viewer_text($row['content'], $row['format'], array('blockwordspreg' => $blockwordspreg));
-			$contentPreview = qa_html(qa_shorten_string_line($text, $maxlength));
+			$contentPreview = $row['title'].". ".qa_html(qa_shorten_string_line($text, $maxlength));
 			$tagtable .= '
 
 				<tr data-original="'.$row['postid'].'">
-				<td><a class="tooltipS" title="'.$contentPreview.'" target="_blank" href="./'.$row['postid'].'?state=edit-'.$row['postid'].'">'.$row['postid'].'</a>
-				<td><div class="post_title_td"><input class="post_title"  value="'.htmlspecialchars($row['title'], ENT_QUOTES, "UTF-8").'" /></div></td> 
-				<td style="width:60%"><div class="post_tags_td"><input class="post_tags"  value="'.$row['tags'].'"   name="q" id="tag_edit_'.$row['postid'].'" autocomplete="off" placeholder="Tags" onkeyup="qa_tag_edit_hints('.$row['postid'].')" onmouseup="qa_tag_edit_hints('.$row['postid'].')" /></div>
+				<td>'.++$rowcnt.'</td>
+				<td>
+<div style="display:none" id="h-'.$row['postid'].'">'.$contentPreview.'</div>
+<a class="tooltips" data-toggle="tooltip" id="'.$row['postid'].'" title="'.$contentPreview.'" target="_blank" href="./'.$row['postid'].'?state=edit-'.$row['postid'].'">'.$row['postid'].'</a></td>
+				<td><div class="post_answer_td" data-postid="'.$row['postid'].'"><input class="post_answer" value ="'.$row['answer'].'" /></div></td>
+
+				<td><div class="post_title_td" data-postid="'.$row['postid'].'"><input class="post_title" value="'.htmlspecialchars($row['title'], ENT_QUOTES, "UTF-8").'" /></div></td> 
+				<td>'.$row['category'].'
+				</td>
+				<td style="width:60%"><div class="post_tags_td" data-postid="'.$row['postid'].'"><input class="post_tags" value="'.$row['tags'].'"   name="q" id="tag_edit_'.$row['postid'].'" autocomplete="off" placeholder="Tags" onkeyup="qa_tag_edit_hints('.$row['postid'].')" onmouseup="qa_tag_edit_hints('.$row['postid'].')" /></div>
 
 				<div class="qa-form-tall-note2">
 				<span id="tag_edit_examples_title_'.$row['postid'].'" style="display:none;"> </span>
 				<span id="tag_edit_complete_title_'.$row['postid'].'" style="display:none;"></span>
-				<span id="tag_edit_hints_'.$row['postid'].'" class="tag_examples"></span></div>
+				<span id="tag_edit_hints_'.$row['postid'].'"></span></div>
 				</td>
 				</tr>';
 		}
@@ -160,187 +289,9 @@ class q2apro_quickedit {
 		$qa_content['custom'.++$c]='<p>'.qa_lang_html('q2apro_quickedit_lang/edit_hint_q').'</p>';
 		$qa_content['custom'.++$c]= $tagtable;
 
-		// make newest users list bigger on page
-		$qa_content['custom'.++$c] = '
-			<style type="text/css">
-			.qa-sidepanel {
-display:none;
-			}
-		.qa-main {
-width:100%;
-		}
-		table {
-width:90%;
-background:#EEE;
-margin:30px 0 15px;
-       text-align:left;
-       border-collapse:collapse;
-		}
-		table th {
-padding:4px;
-background:#cfc;
-border:1px solid #CCC;
-       text-align:center;
-		}
-		table tr:nth-child(even){
-background:#EEF;
-		}
-		table tr:nth-child(odd){
-background:#F5F5F5;
-		}
-		table tr:hover {
-background:#FFD;
-		}
-		table th:nth-child(1), table td:nth-child(1) {
-width:60px;
-      text-align:center;
-		}
-		td {
-border:1px solid #CCC;
-padding:1px 10px;
-	line-height:25px;
-		}
-		table.tagtable td a { 
-			font-size:12px;
-		}
-		input.post_title, input.post_tags, .inputdefault {
-width:100%;
-border:1px solid transparent;
-padding:3px;
-background:transparent;
-		}
-		input.post_title:focus, input.post_tags:focus, .inputactive {
-background:#FFF !important;
-	   box-shadow:0 0 2px #7AF
-		}
-		.post_title_td, .post_tags_td {
-position:relative;
-		}
-		.sendr,.sendrOff {
-padding:3px 10px;
-background:#FC0;
-border:1px solid #FEE;
-       border-radius:2px;
-position:absolute;
-right:-77px;
-top:-5px;
-color:#123;
-cursor:pointer;
-		}
-		.sendrOff {
-			text-decoration:none !important;
-		}
-		</style>';
 
-		$qa_content['custom'.++$c] = '
-			<script type="text/javascript">
-			$(document).ready(function(){
-					var recentTR = null;
-				
-					$(document).click( function(e) {
-						var focused = $(e.target);	
-							var currentTR =  null;
-							if(focused.parent().hasClass("qa-tag-link")) return;
-							if(focused.hasClass("post_title") || focused.hasClass("post_tags"))
-								 currentTR = focused.parent().parent().parent();
-							var postid1 = null, postid2 = null;
-							if(recentTR) postid1 = recentTR.attr("data-original"); 
-							if(currentTR) postid2 = currentTR.attr("data-original"); 
-							if(recentTR && ((postid1 !== postid2)))
-							{
-								console.log("rec = "+postid1);
-								console.log("cur = " +postid2);
-								doAjaxPost(recentTR);
-								recentTR = currentTR;
-							}
-							else if(recentTR != null){ return;}
-							else recentTR = currentTR;
-							$(".post_title, .post_tags").removeClass("inputactive");
-							if(currentTR) currentTR.find("input.post_title, input.post_tags").addClass("inputactive");
-							// alert(recentTR.find("input.post_tags").val());
-
-							// add Update-Button if not yet added
-							if(currentTR && (currentTR.find(".post_tags_td").has(".sendr").length == 0)) {
-							// remove all other update buttons
-						//	$(".sendr").fadeOut(1500, function(){$(this).remove() });
-							if(currentTR)currentTR.find(".post_tags_td").append("<a class=\'sendr\'>Update</a>");
-							}
-							});
-					$(document).keyup(function(e) {
-							// get focussed element
-							var focused = $(":focus");
-							// if enter key and input field selected
-							if(e.which == 13 && (focused.hasClass("post_title") || focused.hasClass("post_tags"))) { 
-							doAjaxPost(recentTR);
-							}
-							// escape has been pressed
-							else if(e.which == 27) {
-							// remove all Update buttons and unfocus input fields
-							$(".sendr").remove();
-							// remove focus from input field
-							$(":focus").blur();
-							// remove active css class
-							$(".post_title, .post_tags").removeClass("inputactive");									
-							}
-							else if((focused.hasClass("post_title") || focused.hasClass("post_tags"))) { 
-							if(recentTR == null) recentTR = focused.parent().parent().parent();
-							}
-							});
-					$(document).on("click", ".sendr", function() {
-							doAjaxPost(recentTR);
-							});
-					function doAjaxPost(recentTR, auto=false) {
-						// get post data from <tr> element
-						var postid = recentTR.attr("data-original"); 
-						var posttitle = recentTR.find("input.post_title").val();
-						var posttags = recentTR.find("input.post_tags").val();
-						// alert(postid + " | " + posttitle + " | " + posttags);
-						// var senddata = "postid="+postid+"&title="+posttitle+"&tags="+posttags;
-						recentTR.find("#tag_edit_hints_"+postid).fadeOut(100, function(){$(this).innerHTML = "";$(this).style.visibility="hidden"; });
-						var dataArray = {
-postid: postid,
-	title: posttitle,
-	tags: posttags
-						};
-						var senddata = JSON.stringify(dataArray);
-						console.log("sending: "+senddata);
-						// send ajax
-						$.ajax({
-type: "POST",
-url: "'.qa_self_html().'",
-data: { ajaxdata: senddata },
-dataType:"json",
-cache: false,
-success: function(data) {
-//dev
-console.log("server returned:"+data+" #Tags: "+data["tags"]);
-
-// prevent another click on button by assigning another class id
-$(".sendr").attr("class","sendrOff");
-// show success indicator checkmark
-recentTR.find(".sendrOff").css("background", "#55CC55");
-recentTR.find(".sendrOff").html("<span style=\'font-size:150%;\'>&check;</span>");
-
-// write title back to posttitle input field
-recentTR.find("input.post_title").val(data["title"]);
-// write tags back to tags input field
-recentTR.find("input.post_tags").val(data["tags"]);
-
-// remove update button
-recentTR.find(".sendrOff").fadeOut(600, function(){$(this).remove() });
-// remove focus from input field
-recentTR.find(":focus").blur();
-// remove active css class
-recentTR.find(".post_title, .post_tags").removeClass("inputactive");									
-}
-});
-}
-});
-
-</script>';
-
-return $qa_content;
-}
+		return $qa_content;
+	}
 
 };
 
